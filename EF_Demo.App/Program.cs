c93bdcf4f5c13363c4cf7d5d42eea70227efffdd
Starinin Andrey <starininandrey@gmail.com>
2022-06-05 10:53:14 +0300

    Add CRUD

--- EF_Demo.App/Program.cs
+++ EF_Demo.App/Program.cs
@@ -1,4 +1,5 @@
 ﻿using EF_Demo.DB;
+using Microsoft.EntityFrameworkCore;
 
 namespace EF_Demo.App
 {
@@ -7,15 +8,20 @@ namespace EF_Demo.App
         private static void Main(string[] args)
         {
             var db = new DataBase();
+            AddUsers(db);
+            PrintUsers(db.Users);
+            Console.WriteLine();
 
-            db.Users.Add(new User
-            {
-                FirstName = "Andrey",
-                LastName = "Starinin",
-                Age = 36
-            });
+            var user = db.Users.First(u => u.Id == 4);
+            db.Users.Remove(user);
+            //db.Users.Remove(db.Users.First(u => u.Id == 4));
             db.SaveChanges();
-            
+            PrintUsers(db.Users);
+            Console.WriteLine();
+
+            user = db.Users.First(u => u.FirstName == "User 2");
+            user.Age = 30;
+            db.SaveChanges();
             PrintUsers(db.Users);
         }
 
@@ -25,6 +31,19 @@ namespace EF_Demo.App
             {
                 Console.WriteLine($"#{user.Id}: {user.LastName} {user.FirstName}, {user.Age}");
             }
+        }
+
+        private static void AddUsers(DataBase db)
+        {
+            db.Users.AddRange(
+                new User("User 1", "Users", 20),
+                new User("User 2", "Users", 20),
+                new User("User 3", "Users", 20),
+                new User("User 4", "Users", 20),
+                new User("User 5", "Users", 20),
+                new User("User 6", "Users", 20)
+            );
+            db.SaveChanges();
         }
     }
 }
--- EF_Demo.DB/User.cs
+++ EF_Demo.DB/User.cs
@@ -6,6 +6,22 @@ public record User
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public int Age { get; set; }
+
+    public User() { }
+    public User(string? firstName, string? lastName, int age)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+    }
+
+    public User(int id, string? firstName, string? lastName, int age)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+    }
 }
 
 // public record User(int Id, string? FirstName, string? LastName, int Age);
